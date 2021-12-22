a49538e6fdf705d5e644a7f31e7c3e3ee33ffa88
Sion <sh1670@bristol.ac.uk>
2021-12-22 12:24:21 +0000

    moved stuff from BB to here and removed iframes so now just OOP. Have made repo private until it is ready

---
+++ coms10017.js
@@ -0,0 +1,17 @@
+//UNIT DETAILS
+var unitName      = "Object Oriented Programming and Algorithms I";
+var unitCode      = "COMS10017";
+var startWeekNum  = 13;                         // start week number [usual start is week 1 or 13]
+var totalWeeks    = 12;                          // total teaching weeks for unit [usually 12]
+
+//PAGE TITLE CONTENT (IF ANY)
+var topicsNum = 7; // num of topics to render in title, that is 1 plus max index, -1 for no topics
+var topics = [
+[  "0", "0", "OVERVIEW",               "<strong>Unit Overview.</strong> This unit provides an introduction to object-oriented programming and to the design and analysis of algorithms. It is delivered in two distinct streams each focusing on one of the subjects. The unit assumes no prior exposure to either of the two subjects. For object-oriented programming, the aim is to introduce objects and classes. Further important principles include inheritance, abstraction, encapsulation, polymorphism, and design patterns. For the design and analysis of algorithms, the aim is to introduce students to some fundamental algorithms and data structures, and the basics of algorithmic design and analysis."],
+[  "1", "0", "STAFF AND TAs",                  "<strong>Academic Teaching Staff:</strong> <a href='https://seis.bristol.ac.uk/~sh1670' target='_blank'>Sion Hannuna (Unit Director)</a>, <a href='http://people.cs.bris.ac.uk/~konrad' target='_blank'>Christian Konrad</a>, <a href='http://people.cs.bris.ac.uk/~burghard' target='_blank'>Tilo Burghardt</a><br/><strong>TAs:</strong> Ethan Bannister,Mutalib Mohammed,Ayodya Samarawickrama,Charlie Walpole,Du Guoyuan,George Sains,Robert Gabriel Popescu,Taharka Okai,Timothy Morgan,Valentin Oltyan,Valentin Papa,Tom Lin (Lead TA),Yiteng Shen"],
+[  "2", "0", "BUBBLES",                "<strong>Learning Bubbles.</strong> As a student in this unit you are part of a 'Learning Bubble', which is a group of students who work more closely together in their learning. There are 24 'Learning Bubbles' overall. During lab session we expect you to meet live in Mircosoft Teams with all your peers in the channel associated to your Bubble (go to the channel and click 'Join' the meeting or click on 'Meet' if you are the first student starting the meeting). You should help each other during all formative courseworks. However, summative courseworks are to be completed in pairs - do not copy any code from outside your programming pair and do not share any code of your programming team to avoid plagiarism. Find below an overview about which student (identified by username) is in which 'Learning Bubble' number:<br/><img src='coms10017_bubbles.jpg' width='100%'></img>"],
+[  "3", "1", "OBJECT ORIENTED TEAM",   "https://teams.microsoft.com/l/channel/19%3a3986403e9da04e3698616ade16c3b314%40thread.tacv2/General?groupId=03f88849-4926-48a5-be2d-06212fe3a26a&tenantId=b2e47f30-cd7d-4a4e-a5da-b18cf1a4151b"],
+[  "4", "1", "BLACKBOARD",             "https://www.ole.bris.ac.uk/webapps/blackboard/execute/content/blankPage?cmd=view&content_id=_5147643_1&course_id=_240777_1"],
+[  "5", "1", "ATTENDANCE",             "https://www.ole.bris.ac.uk/webapps/blackboard/execute/blti/launchPlacement?blti_placement_id=_220_1&content_id=_4919935_1&course_id=_240777_1"],
+[  "6", "1", "CATALOGUE",              "https://www.bris.ac.uk/unit-programme-catalogue/UnitDetails.jsa?ayrCode=20%2F21&unitCode=COMS10017"],
+];
