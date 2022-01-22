3e7891bea03f1c3d48b08a3165993ba2a7bfbd9d
TiloBurghardt <tb2935@bristol.ac.uk>
2022-01-22 13:55:18 +0000

    Update coms10017.js

--- coms10017.js
+++ coms10017.js
@@ -1,13 +1,14 @@
 //UNIT DETAILS
-var unitName      = "Object Oriented Programming and Algorithms I";
+var unitName      = "Object Oriented Programming Component";
 var unitCode      = "COMS10017";
 var startWeekNum  = 13;                         // start week number [usual start is week 1 or 13]
 var totalWeeks    = 1;                          // total teaching weeks for unit [usually 12]
 
 //PAGE TITLE CONTENT (IF ANY)
-var topicsNum = 3; // num of topics to render in title, that is 1 plus max index, -1 for no topics
+var topicsNum = 4; // num of topics to render in title, that is 1 plus max index, -1 for no topics
 var topics = [
-[  "0", "0", "OVERVIEW",               "<strong>Unit Overview.</strong> This unit provides an introduction to object-oriented programming and to the design and analysis of algorithms. It is delivered in two distinct streams each focusing on one of the subjects. The unit assumes no prior exposure to either of the two subjects. For object-oriented programming, the aim is to introduce objects and classes. Further important principles include inheritance, abstraction, encapsulation, polymorphism, and design patterns. For the design and analysis of algorithms, the aim is to introduce students to some fundamental algorithms and data structures, and the basics of algorithmic design and analysis."],
-[  "1", "0", "STAFF",                  "<strong>Academic Teaching Staff:</strong> <a href='https://seis.bristol.ac.uk/~sh1670' target='_blank'>Sion Hannuna (Unit Director)</a>, <a href='http://people.cs.bris.ac.uk/~konrad' target='_blank'>Christian Konrad</a>, <a href='http://people.cs.bris.ac.uk/~burghard' target='_blank'>Tilo Burghardt</a>"],
-[  "2", "1", "CATALOGUE",              "https://www.bris.ac.uk/unit-programme-catalogue/UnitDetails.jsa?ayrCode=21%2F22&unitCode=COMS10017"],
+[  "0", "0", "OVERVIEW",               "<strong>Unit Overview.</strong> This COMS10017 unit provides an introduction to object-oriented programming and to the design and analysis of algorithms. It is delivered in two distinct streams each focusing on one of the subjects. The unit assumes no prior exposure to either of the two subjects. For object-oriented programming, the aim is to introduce objects and classes. Further important principles include inheritance, abstraction, encapsulation, polymorphism, and design patterns. For the design and analysis of algorithms, the aim is to introduce students to some fundamental algorithms and data structures, and the basics of algorithmic design and analysis. This page is about the Object Oriented Programming component of the unit."],
+[  "1", "0", "STAFF",                  "<strong>Academic Teaching Staff:</strong> <a href='https://seis.bristol.ac.uk/~sh1670' target='_blank'>Sion Hannuna (Unit Director)</a> and <a href='http://people.cs.bris.ac.uk/~burghard' target='_blank'>Tilo Burghardt</a>"],
+[  "2", "1", "GO TO ALGORITHMS COMPONENT", "http://people.cs.bris.ac.uk/~konrad/courses/2021_2022_COMS10017/coms10017.html"],
+[  "3", "1", "CATALOGUE",              "https://www.bris.ac.uk/unit-programme-catalogue/UnitDetails.jsa?ayrCode=21%2F22&unitCode=COMS10017"],
 ];
