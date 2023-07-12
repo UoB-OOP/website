28c8f6bedabf1e6f063f152ee36f34f74c51c35a
sh1670 <sh1670@bristol.ac.uk>
2023-07-12 15:51:12 +0100

    website for 22-23 added

--- 2021/coms10017.js
+++ 2021/coms10017.js
@@ -2,12 +2,11 @@
 var unitName      = "Object Oriented Programming and Algorithms I";
 var unitCode      = "COMS10017";
 var startWeekNum  = 13;                         // start week number [usual start is week 1 or 13]
-var totalWeeks    = 12;                          // total teaching weeks for unit [usually 12]
+var totalWeeks    = 10;                          // total teaching weeks for unit [usually 12]
 
 //PAGE TITLE CONTENT (IF ANY)
-var topicsNum = 3; // num of topics to render in title, that is 1 plus max index, -1 for no topics
+var topicsNum = 2; // num of topics to render in title, that is 1 plus max index, -1 for no topics
 var topics = [
 [  "0", "0", "OVERVIEW",               "<strong>Unit Overview.</strong> This COMS10017 unit provides an introduction to object-oriented programming and to the design and analysis of algorithms. It is delivered in two distinct streams each focusing on one of the subjects. The unit assumes no prior exposure to either of the two subjects. For object-oriented programming, the aim is to introduce objects and classes. Further important principles include inheritance, abstraction, encapsulation, polymorphism, and design patterns. For the design and analysis of algorithms, the aim is to introduce students to some fundamental algorithms and data structures, and the basics of algorithmic design and analysis."],
-[  "1", "1", "TEAMS LINK",           "https://teams.microsoft.com/l/team/19%3ankDP6M4B4FeTllMUbMn_VpDWTLS8DKfqRGc7av00Lr01%40thread.tacv2/conversations?groupId=295c52ee-746c-47aa-91f0-046c739fec46&tenantId=b2e47f30-cd7d-4a4e-a5da-b18cf1a4151b"],
-[  "2", "2", "CATALOGUE",              "https://www.bris.ac.uk/unit-programme-catalogue/UnitDetails.jsa?ayrCode=22%2F23&unitCode=COMS10017"],
+[  "1", "1", "CATALOGUE",              "https://www.bris.ac.uk/unit-programme-catalogue/UnitDetails.jsa?ayrCode=21%2F22&unitCode=COMS10017"],
 ];
