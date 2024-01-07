268ca3749c3506e16c6191bd3f90a9a93aeadfb7
Sion <sh1670@bristol.ac.uk>
2024-01-07 19:35:36 +0000

    Catalogue and Teams link updated

--- coms10017.js
+++ coms10017.js
@@ -8,6 +8,6 @@ var totalWeeks    = 2;                          // total teaching weeks for unit
 var topicsNum = 3; // num of topics to render in title, that is 1 plus max index, -1 for no topics
 var topics = [
 [  "0", "0", "OVERVIEW",               "<strong>Unit Overview.</strong> This COMS10017 unit provides an introduction to object-oriented programming and to the design and analysis of algorithms. It is delivered in two distinct streams each focusing on one of the subjects. The unit assumes no prior exposure to either of the two subjects. For object-oriented programming, the aim is to introduce objects and classes. Further important principles include inheritance, abstraction, encapsulation, polymorphism, and design patterns. For the design and analysis of algorithms, the aim is to introduce students to some fundamental algorithms and data structures, and the basics of algorithmic design and analysis."],
-[  "1", "1", "TEAMS LINK",           "https://teams.microsoft.com/l/team/19%3ankDP6M4B4FeTllMUbMn_VpDWTLS8DKfqRGc7av00Lr01%40thread.tacv2/conversations?groupId=295c52ee-746c-47aa-91f0-046c739fec46&tenantId=b2e47f30-cd7d-4a4e-a5da-b18cf1a4151b"],
-[  "2", "2", "CATALOGUE",              "https://www.bris.ac.uk/unit-programme-catalogue/UnitDetails.jsa?ayrCode=22%2F23&unitCode=COMS10017"],
+[  "1", "1", "TEAMS LINK",           "https://teams.microsoft.com/l/team/19%3A0uEeC9wkdQCW6ktmAbFn57OI5JrWGO54w2TGzwcVbgY1%40thread.tacv2/conversations?groupId=4a84c8b2-8c5d-410f-901f-29b428d5fd3c&tenantId="],
+[  "2", "2", "CATALOGUE",              "https://www.bris.ac.uk/unit-programme-catalogue/UnitDetails.jsa?ayrCode=23%2F24&unitCode=COMS10017"],
 ];
